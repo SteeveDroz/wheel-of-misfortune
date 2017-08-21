6d1073866b0998d2114984f023614b149305921b
Steeve Droz <steeve.droz@rpn.ch>
2017-08-21 11:26:48 +0200

    Add marker

--- wheel.js
+++ wheel.js
@@ -1,9 +1,22 @@
+const SIZE = 800
 let canvas
 let wheel
 
 $(function() {
     canvas = $('#wheel')
-    canvas[0].width = 800
-    canvas[0].height = 800
+    canvas[0].width = SIZE
+    canvas[0].height = SIZE
     wheel = canvas[0].getContext('2d')
+
+    wheel.beginPath()
+    wheel.arc(SIZE / 2, SIZE / 2, SIZE / 2 - 50, 0, 2 * Math.PI, false)
+    wheel.fill()
+
+    wheel.beginPath()
+    wheel.moveTo(SIZE - 100, SIZE / 2)
+    wheel.lineTo(SIZE - 10, SIZE / 2 - 10)
+    wheel.lineTo(SIZE, SIZE / 2)
+    wheel.lineTo(SIZE - 10, SIZE / 2 + 10)
+    wheel.fillStyle = '#f00'
+    wheel.fill()
 })
