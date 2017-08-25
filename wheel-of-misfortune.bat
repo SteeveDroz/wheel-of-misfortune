a31941c9e0365371bc462ee1fe78824e8a007477
Steeve Droz <steeve.droz@rpn.ch>
2017-08-25 07:43:42 +0200

    Add a message about PATH and PHP

--- wheel-of-misfortune.bat
+++ wheel-of-misfortune.bat
@@ -1,3 +1,10 @@
 @echo off
-start http://localhost:3000
-php -S localhost:3000
+where php
+if %ERRORLEVEL% neq 0 (
+    echo Be sure to have php7.0+ in your PATH variable. If you don't know how to do, follow this link: https://lmgtfy.com/?q=add+php+to+path+variable
+    pause
+)
+else (
+    start http://localhost:3000
+    php -S localhost:3000
+)
