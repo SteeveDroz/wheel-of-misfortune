429c4befce8982aea447df07a0239d0c523ee6e2
Steeve Droz <steeve.droz@rpn.ch>
2017-08-28 08:47:50 +0200

    Fix error in .bat

--- wheel-of-misfortune.bat
+++ wheel-of-misfortune.bat
@@ -4,7 +4,7 @@ if %ERRORLEVEL% neq 0 (
     echo Be sure to have php7.0+ in your PATH variable. If you don't know how to do, follow this link: https://lmgtfy.com/?q=add+php+to+path+variable
     pause
 )
-else (
+if %ERRORLEVEL%==0 (
     start http://localhost:3000
     php -S localhost:3000 -t resources
 )
