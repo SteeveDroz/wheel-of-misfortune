10d2a68db74d35d72c318e4ccc4964f20af87ace
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-09-25 13:54:27 +0200

    Set icons for app executables

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
-        icon: 'assets/logo.png'
+        icon: `${__dirname}/assets/logo.png`
     })
     mainWindow.maximize()
 
